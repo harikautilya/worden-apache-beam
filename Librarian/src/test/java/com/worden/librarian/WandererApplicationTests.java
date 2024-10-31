38c3963804f113b8f86c336a0087ff18e17d7572
Kautilyahari <harikautilya@gmail.com>
2024-10-31 13:00:38 +0530

    All project boot strapped

---
+++ Librarian/src/test/java/com/worden/librarian/WandererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worden.librarian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WandererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Wanderer/processor/src/main/java/com/worden/processor/ProcessorOptions.java
+++ Wanderer/processor/src/main/java/com/worden/processor/ProcessorOptions.java
@@ -1,7 +1,10 @@
 package com.worden.processor;
 
 import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.Validation;
 
 public interface ProcessorOptions extends PipelineOptions {
+
+    @Validation.Required
 
 }
